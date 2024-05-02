210ea37a5ff52c384e7dd8b980ba825619c5cac7
Rej <rej.safranko@gmail.com>
2024-05-02 19:08:10 +0200

    restructure

--- .github/ISSUE_TEMPLATE/project-managment.md
+++
@@ -1,7 +0,0 @@
-### Project Managment<br>
-**TODO List**<br>
-1. Download TACO dataset. {DONE}<br>
-2. Research environment container for Colab.
-3. Prepare code for google cloud runtime.<br>
-4. Set up cloud environment.<br>
-5. Run and debug instance segmentation on TACO-10 with MaskRCNN. 
---
+++ src/Trainer.py
@@ -0,0 +1,11 @@
+from detectron2.engine import DefaultTrainer
+from detectron2.evaluator import COCOEvaluator
+import os
+
+
+class Trainer(DefaultTrainer):
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
