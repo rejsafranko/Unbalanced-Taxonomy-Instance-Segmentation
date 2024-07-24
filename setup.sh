15024e8f71742283230daa1d6a65a39eda1d41c9
rejsafranko <rej.safranko@gmail.com>
2024-07-24 08:52:57 +0200

    dir return

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 # Install required Python packages.
-python -m pip install requirements.txt
+python -m pip install -r requirements.txt
 
 # Install DETECTRON2, MASK2FORMER, FC-CLIP in the modules directory.
 cd modules
 python -m pip install "git+https://github.com/facebookresearch/detectron2.git"
+
+cd ..
