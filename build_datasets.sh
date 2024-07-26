fe08f733becc5be82455120a2bfecec6bb3e0697
rejsafranko <rej.safranko@gmail.com>
2024-07-26 12:38:56 +0200

    dataset building condition

--- build_datasets.sh
+++ build_datasets.sh
@@ -1,5 +1,5 @@
-python download.py
-python split_dataset.py --dataset_dir data/annotations
-python map_classes.py --annotation_path data/annotations/annotations_0_train.json --mapping_path data/mappings/map_10.csv
-python map_classes.py --annotation_path data/annotations/annotations_0_val.json --mapping_path data/mappings/map_10.csv
-python map_classes.py --annotation_path data/annotations/annotations_0_test.json --mapping_path data/mappings/map_10.csv
+python src/data/download.py
+python src/data/split_dataset.py --dataset_dir data/annotations
+python src/data/map_classes.py --annotation_path data/annotations/annotations_0_train.json --mapping_path data/mappings/map_10.csv
+python src/data/map_classes.py --annotation_path data/annotations/annotations_0_val.json --mapping_path data/mappings/map_10.csv
+python src/data/map_classes.py --annotation_path data/annotations/annotations_0_test.json --mapping_path data/mappings/map_10.csv
--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 cd modules
 git clone "https://github.com/facebookresearch/detectron2.git"
+git clone "https://github.com/bytedance/fc-clip.git"
 cd ..
 
 pip install -r requirements.txt
-pip install -e modules/detectron2
+#pip install -e modules/detectron2
 
-source ./build_datasets.sh
+if [ $(ls data/annotations | wc -l) -eq 1 ]; then
+    source ./build_datasets.sh
+else
+    echo "build_datasets.sh not run: There are either zero or more than one files in data/annotations"
+fi
