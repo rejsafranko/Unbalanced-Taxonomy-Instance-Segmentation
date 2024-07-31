61b212362016dab0a55041021c5eb2c6ad2c31de
rejsafranko <rej.safranko@gmail.com>
2024-07-31 11:36:10 +0200

    fcclip mvp

--- src/models/Trainers.py
+++ src/models/Trainers.py
@@ -35,3 +35,23 @@ class Mask2FormerTrainer(detectron2.engine.DefaultTrainer):
         if cfg.INPUT.DATASET_MAPPER_NAME == "mask_former_instance":
             mapper = mask2former.MaskFormerInstanceDatasetMapper(cfg, True)
             return detectron2.data.build_detection_train_loader(cfg, mapper=mapper)
+
+
+class FCCLIPTrainer(detectron2.engine.DefaultTrainer):
+    """
+    Extension of the Trainer class adapted to FC-CLIP.
+    """
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        return detectron2.evaluator.COCOEvaluator(
+            dataset_name, cfg, True, output_folder
+        )
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        if cfg.INPUT.DATASET_MAPPER_NAME == "mask_former_instance":
+            mapper = mask2former.MaskFormerInstanceDatasetMapper(cfg, True)
+            return detectron2.data.build_detection_train_loader(cfg, mapper=mapper)
